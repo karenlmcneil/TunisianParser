f5d8ff19197ee8b105aab0dd916d23b04591a20a
karenlmcneil <km1542@georgetown.edu>
2019-04-24 22:15:06 -0400

    Refactored out unnecessary Parse class.

--- parser/parser.py
+++ parser/parser.py
@@ -27,58 +27,54 @@ particles = ['باش', 'انت', 'اللي', 'التي', 'الذي', 'الذين
 
 saved_parses = {}  # TODO: Save to file
 
-for part in particles:
-    saved_parses[part] = (part, 'PART')
 
-
-class Parser:
-    """Object for parsing Tunisian Arabic strings"""
-
-    def __init__(self, string):
-        self.string = string
-        self.parsed_list = []
-
-    def parse(self):
-
-        if self.string in saved_parses.values():
-            return saved_parses[self.string]
-
-        tokens = nltk.tokenize.wordpunct_tokenize(self.string)  # TODO: Need to add preprocessing
-
-        for word in tokens:
-            if not word.isalpha():
-                self.parsed_list.append((word, 'PUNCT'))
-                continue
-            if test_lang(word) != 'AR':
-                self.parsed_list.append((word, 'FW'))
-                continue
-            if word in saved_parses:
-                self.parsed_list.append(saved_parses[word])
-                continue
-            parse_dict = stemmer(word)
-            # print("\nParse dict: ", parse_dict)
-            parse, pos = choose_best_parse(parse_dict)
-            # print("\nChosen parse: ", parse)
-            pos_list = pos.split('_')
-            # try:
-            #     stem = parse.stem.asList()[0]  # because stem is sometimes a list
-            # except:
-            #     stem = parse.stem
-            mapped = list(zip(parse, pos_list))
-            self.parsed_list.extend(mapped)
-                    # for part in parse.asList():
-                    #     pp = ParsePart(part=part, parse=p)
-                    #     pp.part_bw = uni2buck.transString(part, reverse=True)
-                    #     pp.save()
-        #             else:
-        #                 p.parse = word
-        #                 p.pos = 'FW'
-        #                 p.stem = word
-        #                 parsed_list.append(p.stem)
-        #                 p.save()
-        #                 pp = ParsePart(part=word, parse=p)
-        #                 pp.save()
-        return self.parsed_list
+def parse(string):
+    """
+    Morphologically segments, POS taggs, and lemmatizes string of Tunisian Arabic text.
+    :param string: Tunisian Arabic text
+    :return: list: parses in the format ('word', 'POS')
+    """
+    for part in particles:
+        saved_parses[part] = (part, 'PART')
+    tokens = nltk.tokenize.wordpunct_tokenize(string)  # TODO: Need to add preprocessing
+    parsed_list = []
+    for word in tokens:
+        if word in saved_parses.keys():
+            parsed_list.append(saved_parses[word])
+            continue
+        if not word.isalpha():
+            parsed_list.append((word, 'PUNCT'))
+            continue
+        if test_lang(word) != 'AR':
+            parsed_list.append((word, 'FW'))
+            continue
+        if word in saved_parses:
+            parsed_list.append(saved_parses[word])
+            continue
+        parse_dict = stemmer(word)
+        # print("\nParse dict: ", parse_dict)
+        parse, pos = choose_best_parse(parse_dict)
+        # print("\nChosen parse: ", parse)
+        pos_list = pos.split('_')
+        # try:
+        #     stem = parse.stem.asList()[0]  # because stem is sometimes a list
+        # except:
+        #     stem = parse.stem
+        mapped = list(zip(parse, pos_list))
+        parsed_list.extend(mapped)
+                # for part in parse.asList():
+                #     pp = ParsePart(part=part, parse=p)
+                #     pp.part_bw = uni2buck.transString(part, reverse=True)
+                #     pp.save()
+    #             else:
+    #                 p.parse = word
+    #                 p.pos = 'FW'
+    #                 p.stem = word
+    #                 parsed_list.append(p.stem)
+    #                 p.save()
+    #                 pp = ParsePart(part=word, parse=p)
+    #                 pp.save()
+    return parsed_list
 
 
 if __name__ == '__main__':
@@ -86,4 +82,4 @@ if __name__ == '__main__':
     argparser.add_argument("--string", type=str,
                            help="string to parse")
     args = argparser.parse_args()
-    Parser(args.string)
+    parse(args.string)
--- parser/tests/test_parser.py
+++ parser/tests/test_parser.py
@@ -1,70 +1,61 @@
 from unittest import TestCase
-from parser.parser import Parser
+from parser import parser as p
 
 
 class TestParser(TestCase):
 
     def test_existing_parse(self):
         string = 'باش'
-        p = Parser(string)
-        parse = p.parse()
+        parse = p.parse(string)
         expected_output = [('باش', 'PART')]
         self.assertEqual(expected_output, parse)
 
     def test_single_word(self):
         string = 'الكتاب'
-        p = Parser(string)
-        parse = p.parse()
+        parse = p.parse(string)
         expected_output = [('ال', 'DET'), ('كتاب', 'N')]
         self.assertEqual(expected_output, parse)
 
     def test_multiword_string(self):
         string = 'هذه العربية وهذه الmixed!'
-        p = Parser(string)
-        parse = p.parse()
+        parse = p.parse(string)
         expected_output = [('هذه', 'PART'), ('ال', 'DET'), ('عربية', 'N'), ('و', 'C'),
             ('هذه', 'UNIN'), ('ال', 'DET'), ('mixed', 'N'), ('!', 'PUNCT')]
         self.assertEqual(expected_output, parse)
 
     def test_multiple_prefix(self):
         string = 'والكتاب'
-        p = Parser(string)
-        parse = p.parse()
+        parse = p.parse(string)
         expected_output = [('و', 'C'), ('ال', 'DET'), ('كتاب', 'N')]
         self.assertEqual(expected_output, parse)
 
     def test_past_verb(self):
         string = 'كتبت'
-        p = Parser(string)
-        parse = p.parse()
+        parse = p.parse(string)
         expected_output = [('كتبت', 'VBD')]
         self.assertEqual(expected_output, parse)
 
     def test_past_verb_defective(self):
         string = 'مشيت'
-        p = Parser(string)
-        parse = p.parse()
+        parse = p.parse(string)
         expected_output = [('مشيت', 'VBD')]
         self.assertEqual(expected_output, parse)
 
     def test_conj_past_verb(self):
         string = 'وكتبت'
-        p = Parser(string)
-        parse = p.parse()
+        parse = p.parse(string)
         expected_output = [('و', 'C'), ('كتبت', 'VBD')]
         self.assertEqual(expected_output, parse)
 
     def test_present_verb(self):
         string = 'نمشيو'
-        p = Parser(string)
-        parse = p.parse()
+        parse = p.parse(string)
         expected_output = [('نمشيو', 'VBZ')]
         self.assertEqual(expected_output, parse)
 
     def test_conj_present_verb(self):
         string = 'ونمشيو'
-        p = Parser(string)
-        parse = p.parse()
+        parse = p.parse(string)
         expected_output = [('و', 'C'), ('نمشيو', 'VBZ')]
         self.assertEqual(expected_output, parse)
 
@@ -74,7 +65,7 @@ class TestParser(TestCase):
 
     # def test_real_sentences(self):
     #     string = 'ومن وقتاش رجعت تحكي معاه المدير؟'
-    #     p = Parser(string)
+    #
     #     parse = p.parse()
     #     print("parse is ", parse)
     #     expected_output = [('و', 'C'), ('من', 'P'), ('وقتاش', 'PART'), ('رجعت', 'VBD'),
